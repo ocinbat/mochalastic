25832fc524ff41b9732a622bf5c22c4a6247977d
Ömer Cinbat <ocinbat@outlook.com>
2017-09-20 18:28:57 +0300

    Added tests

---
+++ test/mochalastic.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var Chai  = require('chai');
+var Mochalastic = require('./../lib/mochalastic.js');
+var Should = Chai.should();
+
+describe('mochalastic reporter', function () {
+  var stdout;
+  var runner;
+
+  beforeEach(function () {
+    stdout = [];
+    runner = {};
+  });
+
+  describe('on test end', function () {
+    it('should nothing happen', function () {
+      var expectedTitle = 'some title';
+      var test = {
+        fullTitle: function () {
+          return expectedTitle;
+        },
+        slow: function () {},
+        currentRetry: function () {}
+      };
+      runner.on = function (event, callback) {
+        if (event === 'test end') {
+          callback(test);
+        }
+        if (event === 'end') {
+          callback();
+        }
+      };
+      
+      Mochalastic.call({}, runner);
+    });
+  });
+});
